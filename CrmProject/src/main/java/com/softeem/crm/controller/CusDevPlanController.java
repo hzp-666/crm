ce5b400e8136716e76b92efea6760a4de5ab9578
hzp-666 <67034681+hzp-666@users.noreply.github.com>
2023-03-31 15:35:24 +0800

    Update CusDevPlanController.java
    第三次修改

--- CrmProject/src/main/java/com/softeem/crm/controller/CusDevPlanController.java
+++ CrmProject/src/main/java/com/softeem/crm/controller/CusDevPlanController.java
@@ -34,6 +34,7 @@ public class CusDevPlanController extends BaseController {
     public String index() {
         log.info("修改11111111111111111");
         log.info("修改22222222222222222");
+        log.info("修改33333333333333333");
         return "cusDevPlan/cus_dev_plan";
     }
 
